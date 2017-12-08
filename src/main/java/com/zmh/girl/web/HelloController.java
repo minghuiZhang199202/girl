48f198f1320ddfc749d0cc003a99fde45031de33
zhang hui <yuchen199202@163.com>
2017-12-08 22:24:03 +0800

    完成Spring Boot创建web应用的最简单版本

---
+++ src/main/java/com/zmh/girl/web/HelloController.java
@@ -0,0 +1,16 @@
+package com.zmh.girl.web;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author  zmh
+ * date 2017-12-8 22:22:44
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        return "Hello Spring Boot";
+    }
+}
