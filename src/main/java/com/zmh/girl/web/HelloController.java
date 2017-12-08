39526533784ad9b17d1e5185cba174ef96dadf61
zhang hui <yuchen199202@163.com>
2017-12-08 23:22:21 +0800

    学习属性配置    配置文件使用yml格式，Profiles包括开发环境，生产环境。通过切换spring.profiles.active=dev/product,来切换环境，更改配置参数。

--- src/main/java/com/zmh/girl/web/HelloController.java
+++ src/main/java/com/zmh/girl/web/HelloController.java
@@ -1,5 +1,8 @@
 package com.zmh.girl.web;
 
+import com.zmh.girl.model.GirlProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -9,9 +12,10 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class HelloController {
-
+    @Autowired
+    private GirlProperties girlProperties;
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
-        return "Hello Spring Boot";
+        return "Hello Spring Boot " + girlProperties.getContent();
     }
 }
